180be091e6d819890e369c9e8223867067730a7d
Daniel Monarrez <dmonar2@uic.edu>
2019-09-24 15:55:31 -0500

    Added Static file

--- stock.py
+++ stock.py
@@ -9,17 +9,19 @@ app = Flask(__name__)
 def stocks():
     stock_symbol = request.form['ssymbol']
     r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+stock_symbol+'&interval=5min&apikey=7KY3WGUYTHYBYFK6') 
-    json_object = r.json()
-    j_obj = json_object['Time Series (5min)']['2019-08-05 16:00:00']['1. open']
-    return render_template('stocks.html',stock=j_obj)
-
+    json_object = r.text
+    #j_obj = json_object['Time Series (5min)']['2019-08-05 16:00:00']['1. open']
+    #return render_template('stocks.html',stock=j_obj)
+    return json_object
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
 
 # api 7KY3WGUYTHYBYFK6
 # ts = TimeSeries(key='7KY3WGUYTHYBYFK6')
